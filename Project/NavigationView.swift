f25749215ad687043b115b35982407696e1c23ef
Adam Derradji <adamderradji10@gmail.com>
2019-10-16 00:50:18 +0100

    Outlets for navigation view

---
+++ Project/NavigationView.swift
@@ -0,0 +1,31 @@
+//
+//  NavigationView.swift
+//  Project
+//
+//  Created by Adam Derradji on 15/10/2019.
+//  Copyright © 2019 nala. All rights reserved.
+//
+
+import UIKit
+
+class NavigationView: UIView {
+
+  @IBOutlet var candleButtonView: UIView!
+  @IBOutlet var candleButtonWidthConstraint: NSLayoutConstraint!
+  @IBOutlet var candleButtonBottomConstraint: NSLayoutConstraint!
+  
+  @IBOutlet var statsIconView: UIView!
+  @IBOutlet var statsIconWidthConstraint: NSLayoutConstraint!
+  @IBOutlet var statsIconBottomConstraint: NSLayoutConstraint!
+  @IBOutlet var statsIconHorizontalConstraint: NSLayoutConstraint!
+  
+  @IBOutlet var resourcesIconView: UIView!
+  @IBOutlet var resourcesHorizontalConstraint: NSLayoutConstraint!
+  
+  @IBOutlet var indicator: UIView!
+  
+  @IBOutlet var colourView: UIView!
+  
+  
+  
+}
